d4c2a442d95e36831fa061a075d6d9b3fb79052d
Lau Altamirano <lauraltamirano.9@gmail.com>
2022-08-26 23:39:49 -0300

    style

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Header from './components/Header/Header';
 import NavBar from './components/Navegador/NavBar';
 
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div>
       <NavBar/>
+      <Header />
       
     </div>
   );
---
+++ e-commerce/src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import fondo from "../../assets/images/fondo_header.jpg";
+
+const Header = () => {
+    return (
+        <div className="containerHeader">
+
+            <img src={fondo} alt="fondo" />
+
+        </div>
+    )
+};
+
+export default Header;
